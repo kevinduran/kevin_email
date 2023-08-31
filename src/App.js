daab714befa5f2c915cd0fe7ea2d6f7acc28cdb3
Kevin <kevinalexanderduran8@gmail.com>
2023-08-30 19:24:17 -0700

    START: 8.30

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './Nav/Nav';
+import Header from './Header/Header'
 
 function App() {
   return (
     <div className='app'>
       <Nav/>
+      <Header/>
     </div>
   );
 }
